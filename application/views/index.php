d1aafe2ba14c19758a32c4c41ec4c604b96e9b3e
aliefyp <aliefyp@gmail.com>
2015-07-21 07:26:59 +0700

    Konten baru
    Docnya silahkan donlot di email thinker

--- application/views/index.php
+++ application/views/index.php
@@ -137,8 +137,8 @@ $(document).ready(function() {
 <!-- SLIDES START -->   
 	<div id="slide1">
 		<div class="container">
-			<img class="section logo img-responsive" alt="Responsive image" src="assets/img/logo.png">
-	        <img class="section tagline img-responsive" alt="Responsive image" src="assets/img/tagline.png">
+			<img class="section img-responsive" alt="Responsive image" src="assets/img/logo.png">
+	        <h4 style="text-align:center">“Held by the BUBW-Indonesia Committee of Civilizations Exchange and Cooperation Foundation (CECF) and proudly hosted by Bina Antarbudaya Chapter Yogyakarta”</h4>
 		</div> 
     </div> 
 
@@ -150,55 +150,14 @@ $(document).ready(function() {
 	    <div class="container slide">
 	      <div class="row">
 	        <div class="col-md-6 col-sm-12" style="text-align:center">
-	          <h2 class="hl">H I G H L I G H T S</h2>
-	          <iframe width="500" height="260" src="https://www.youtube.com/embed/xkDDESlO5dM" autoplay="1" frameborder="0" allowfullscreen></iframe>
+	          <iframe width="500" height="260" src="https://www.youtube.com/embed/xkDDESlO5dM" frameborder="0" allowfullscreen></iframe>
 	          <img src="assets/img/shadow.png" alt="Responsive image" class="img-responsive shadow ">
 	        </div>
 	        <div class="col-md-6 col-sm-12">
-	          <h3>Recent news</h3>
-	          <div class="row">
-	            <div class="col-md-12 col-lg-12">
-					<ul class="media-list main-list">
-					  <li class="media">
-					    <a class="pull-left" href="#">
-					      <img class="media-object" src="http://placehold.it/150x90" alt="...">
-					    </a>
-					    <div class="media-body">
-					      <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
-					      <p class="by-author">By Jhon Doe</p>
-					    </div>
-					  </li>
-					  <li class="media">
-					    <a class="pull-left" href="#">
-					      <img class="media-object" src="http://placehold.it/150x90" alt="...">
-					    </a>
-					    <div class="media-body">
-					      <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
-					      <p class="by-author">By Jhon Doe</p>
-					    </div>
-					  </li>
-					  <li class="media">
-					    <a class="pull-left" href="#">
-					      <img class="media-object" src="http://placehold.it/150x90" alt="...">
-					    </a>
-					    <div class="media-body">
-					      <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
-					      <p class="by-author">By Jhon Doe</p>
-					    </div>
-					  </li>
-					  <!-- <li class="media">
-					    <a class="pull-left" href="#">
-					      <img class="media-object" src="http://placehold.it/150x90" alt="...">
-					    </a>
-					    <div class="media-body">
-					      <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
-					      <p class="by-author">By Jhon Doe</p>
-					    </div>
-					  </li> -->
-					</ul>
-				</div>
-	          </div>
-	          <a href="#slide2ext" class="btn btn-info btn-lg morenews" style="float:right" role="button" target="_blank">MORE NEWS!</a>
+	          <h2>W E L C O M E</h2>
+	          <p>BUBW-Indonesia is one of the International BUBW that will be one of the most prestigious project events in 2015 with the grand theme: Better Understanding as a Grand Tool to Face the World and South East Asia Challenges.</p>
+	          <p>Indonesia has been chosen since Indonesia is home to numerous different ethnic groups and religions, Indonesia’s cultural diversity remains unmatched in Southeast Asia and even the world, and Indonesia Open Door Foreign Policy might enhance the cultural complexity in Indonesia.</p>
+	          <p>Amidst similarity, BUBW Indonesia is modified from the original one with not only the main conference itself, but call for paper  to enrich and improve the discussion and brainstorming among the participants.  A published resolution will be the goal toward the end of this event. The action will inspire the general public to see that we are not quiet to many conflicts happening right now. Bringing youth from different background together, discussing and synergizing, is a very powerful symbolic voice to the society and that there is hope.</p>
 	        </div>
 	      </div>
 	    </div>	    
@@ -207,20 +166,85 @@ $(document).ready(function() {
     <div id="slide2-2">
     </div> 
 
-    <div id="slide2" style="height:100vh">
+    <div id="slide2">
 	    <div id="slide2ext" class="container slide" style="padding-top:100px;">
 	      <div class="row">
-	            <div class="col-md-6 col-sm-6">
+	            <div class="col-md-8 col-sm-6">
 	              <div class="page-header">
-	                <h1 style="font-size:60px;">H E A D L I N E</h1>
+	                <h1 style="font-size:60px;">A B O U T</h1>
 	              </div>
-	              <p class="headline">BUBW Conferences are held annually in Florida, California and Maryland – USA.
-	              Nowadays International BUBW Conferences are taking aim to spread the message all around the world.
-	              In 2016, International BUBW Conferences will be conducted in Kyrgyzstan for the youth / young professionals of Central Asia, in Turkey for Europe and in Uganda for East Africa.
-	              <br><br><strong>This year, 2015, INDONESIA</strong> will host the International BUBW Conference in December for the youth / young professionals of Southeast Asia.
-	              </p>
+	              <div class="container-fluid">
+				    <div id="custom_carousel" class="carousel slide" data-ride="carousel" data-interval="2500">
+				        <!-- Wrapper for slides -->
+				        <div class="carousel-inner">
+				            <div class="item active">
+				                <div class="container-fluid">
+				                    <div class="row">
+				                        <div class="col-md-3"><img src="assets/img/thumb3.jpg" class="img-responsive thumb"><img src="assets/img/thumb1.jpg" class="img-responsive thumb"></div>
+				                        <div class="col-md-9">
+				                            <h2>BUBW Conference</h2>
+				                            <p>Better Understanding for a Better World (BUBW) Conference began in 2006 with an annual conference Orlando, FL. In 2008, Civilizations Exchange and Cooperation Foundation expanded this unique opportunity and included an annual conference in Baltimore, MD. After two more thriving years of BUBW Conference in both, Orlando and Baltimore, CECF expanded once more to include an annual BUBW Conference in San Diego, CA in 2010. This conferences were developed to give American high school students and international exchange students currently studying in the US the opportunity to meet students of diverse faiths and culture background. They have been tremendously successful each year and the students benefit in ways that are truly impressive.</p>
+				                        	<p>Youth are the hope of each nation, and the future of the world’s civilization is soon to rest in their hands. The most pressing issue they face today is how to view the “other”, people who are different from you. CECF aims to transcend the fear of the “other” through multicultural and interfaith dialogue and mutual respect and understanding.</p>
+				                        </div>
+				                    </div>
+				                </div>            
+				            </div> 
+				            <div class="item">
+				                <div class="container-fluid">
+				                    <div class="row">
+				                        <div class="col-md-12">
+				                            <h2>BUBW's Objectives</h2>
+				                            <ul style="font-size:14px; color:#777">
+				                            	<li>Facilitate mutual understanding and appreciation between students from diverse regional, economic, social, religious, cultural and ethnic backgrounds</li>
+				                            	<li>Learn about and discuss the practice of religion and faith in a pluralistic society with the objective of promoting tolerance, mutual respect, and an awareness of common values</li>
+				                            	<li>Discuss the danger of religious extremism, fanaticism or ethnic nationalism, which has historically been the recipe for wars and conflicts in many civilizations</li>
+				                            	<li>Introduce various aspects of leadership in the age of a "global village," including those involving business and entrepreneurship</li>
+				                            	<li>Forge ties of friendship between the participants, who are without doubt, the future leaders of tomorrow</li>
+				                            </ul>
+				                        </div>
+				                    </div>
+				                </div>            
+				            </div> 
+				            <div class="item">
+				                <div class="container-fluid">
+				                    <div class="row">
+				                        <div class="col-md-4"><img src="assets/img/thumb4.jpg" class="img-responsive"></div>
+				                        <div class="col-md-8">
+				                            <h2>International Better Understanding for a Better World (BUBW) Conferences for university students and young professionals</h2>
+				                            <p>An international BUBW Conference will be held in various regions such as Indonesia, Malaysia, Macedonia, Russia, Egypt, Morocco, Ghana, etc. around the world each year. Alumni of the previous BUBW conferences will be invited to take the lead in this conference for university students and young professionals. Local regional governments will be invited to contribute by sending their high achieving their youths to participate also.</p>
+				                        </div>
+				                    </div>
+				                </div>           
+				            </div> 
+				            <div class="item">
+				                <div class="container-fluid">
+				                    <div class="row">
+				                        <div class="col-md-3"><img src="assets/img/cecf.png" class="img-responsive"></div>
+				                        <div class="col-md-9">
+				                            <h2>Civilizations Exchange and Cooperation Foundation (CECF)</h2>
+				                            <p>Civilizations Exchange and Cooperation Foundation (CECF) is determined to foster cooperation rather than confrontation among people of different religions and cultures. CECF focuses on factors that led to the rise and fall of previous civilizations and how people of this millennium can benefit from lessons of the past in a world literally changing to a global village. The International Better Understanding for a Better World (BUBW-Indonesia)Conference is an adaptation from the Better Understanding for a Better World (BUBW) Conferences in the U.S. and has been approved by Civilizations Exchange and Cooperation Foundation (CECF).</p>
+											<p>See more at:<br />
+											<a href="www.bubw.org">www.bubw.org</a>, <a href="www.cecf-net.org">www.cecf-net.org</a></p>
+				                        </div>
+				                    </div>
+				                </div>           
+				            </div> 
+				        <!-- End Item -->
+				        </div>
+				        <!-- End Carousel Inner -->
+				        <div class="controls">
+				            <ul class="nav">
+				                <li data-target="#custom_carousel" data-slide-to="0" class="active"><a href="#"><small>BUBW Conference</small></a></li>
+				                <li data-target="#custom_carousel" data-slide-to="1"><a href="#"><small>Objectives</small></a></li>
+				                <li data-target="#custom_carousel" data-slide-to="2"><a href="#"><small>International BUBW</small></a></li>
+				                <li data-target="#custom_carousel" data-slide-to="3"><a href="#"><small>About CECF</small></a></li>
+				            </ul>
+				        </div>
+				    </div>
+				    <!-- End Carousel -->
+				</div>
 	            </div>
-	            <div class="col-md-6 col-sm-6">
+	            <div class="col-md-4 col-sm-6">
 	              <img src="assets/img/dance.png" class="img-responsive giant" alt="Responsive image">
 	            </div>
 	          </div>
@@ -305,15 +329,55 @@ $(document).ready(function() {
 	
 	
 	<div id="slide5">
-		<div class="content">
+		<!-- <div class="content">
            <div class="quotes_container">
 		    <h3 class="quotes">“My idea of Christmas, whether old-fashioned or modern, is very simple: loving others. Come to think of it, why do we have to wait for Christmas to do that?” </h3>
 		    <img src="assets/img/bob.png" align="left"/> <h4 class="author_name_white">Bob Hope </h4>
 		  </div> 
 		<div id="copyright"><a href="http://www.1stwebdesigner.com/">Copyright 1stwebdesigner.com </a></div>  
-		</div> 
-		
+		</div>  -->
+		<div class="container" style="text-align:center">
+			<h1 style="color:#fff">Join Us!</h1>
+			<p class="quotes" style="text-align:center; color:#fff">Register yourself for the greatest experience among the best from generation around the world! Click the Register button, and then fill the requested form.</p>	
+			<a href="#myModal" class="btn btn-info btn-lg morenews" style="backgound-color:transparent; text-align:center; width:30%" role="button" target="_blank">REGISTER NOW!</a>
+			
+			<!-- I got these buttons from simplesharebuttons.com -->
+			<div id="share-buttons">
 
+			    <!-- Email -->
+			    <a href="mailto:?Subject=Simple Share Buttons&amp;Body=I%20saw%20this%20and%20thought%20of%20you!%20 https://simplesharebuttons.com">
+			        <img src="https://simplesharebuttons.com/images/somacro/email.png" alt="Email" />
+			    </a>
+			 
+			    <!-- Facebook -->
+			    <a href="http://www.facebook.com/sharer.php?u=https://simplesharebuttons.com" target="_blank">
+			        <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" />
+			    </a>
+			    
+			    <!-- Google+ -->
+			    <a href="https://plus.google.com/share?url=https://simplesharebuttons.com" target="_blank">
+			        <img src="https://simplesharebuttons.com/images/somacro/google.png" alt="Google" />
+			    </a>
+			    
+			    <!-- LinkedIn -->
+			    <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=https://simplesharebuttons.com" target="_blank">
+			        <img src="https://simplesharebuttons.com/images/somacro/linkedin.png" alt="LinkedIn" />
+			    </a>
+			    
+			    <!-- Tumblr-->
+			    <a href="http://www.tumblr.com/share/link?url=https://simplesharebuttons.com&amp;title=Simple Share Buttons" target="_blank">
+			        <img src="https://simplesharebuttons.com/images/somacro/tumblr.png" alt="Tumblr" />
+			    </a>
+			     
+			    <!-- Twitter -->
+			    <a href="https://twitter.com/share?url=https://simplesharebuttons.com&amp;name=Simple Share Buttons&amp;hashtags=simplesharebuttons" target="_blank">
+			        <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" />
+			    </a>
+			    
+			</div>
+
+		</div>
+		
     </div> 
 
 
@@ -339,12 +403,21 @@ $(document).ready(function() {
 
 
 	
-<!-- SLIDES END -->
-<script>
+		<!-- SLIDES END -->
+		<script>
 			// For Demo purposes only (show hover effect on mobile devices)
 			[].slice.call( document.querySelectorAll('a[href="#"') ).forEach( function(el) {
 				el.addEventListener( 'click', function(ev) { ev.preventDefault(); } );
 			} );
+		</script>
+
+		<script>
+			$(document).ready(function(ev){
+			    $('#custom_carousel').on('slide.bs.carousel', function (evt) {
+			      $('#custom_carousel .controls li.active').removeClass('active');
+			      $('#custom_carousel .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
+			    })
+			});
 		</script>
 </body>
 </html>
