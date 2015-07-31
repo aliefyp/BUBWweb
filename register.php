2f7b4ba5099c7d19522382b7cf596671a0a91462
Reva Yoga Pradana <ryp_reaper@yahoo.com>
2015-07-31 18:45:25 +0700

    membenahi sesuai permintaan alief -changed to register.php - in tag head, changed

--- register.php
+++ register.php
@@ -1,94 +1,20 @@
 <head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>BUBW Indonesia</title>
-		<link rel="shortcut icon" href="assets/img/favicon.png">
-		<!--New Page Transition
-		<link rel="stylesheet" type="text/css" href="assets/newpage/css/component.css" />
-		<script src="assets/newpage/js/modernizr.custom.js"></script>-->
+	<?php
+	$this->load->view('head') ?>
 
-		<!--Bootstrap-->
-		<link rel="stylesheet" href="assets/css/bootstrap.css"/>
-		<link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
-  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-		
-		
+	<style type="text/css">
+	.container {
+	min-height: 100vh;
+	}
+	p {
+	text-align: justify;
+	}
+	#slide1 {
+	padding-top: 10%;
+	}
+	</style>
 
-		<!--Hover Effect-->
-		<link rel="stylesheet" type="text/css" href="assets/hover/css/normalize.css" />
-		<link rel="stylesheet" type="text/css" href="assets/hover/css/demo.css" />
-		<link rel="stylesheet" type="text/css" href="assets/hover/css/set2.css" />
-		<link rel="stylesheet" type="text/css" href="assets/hover/fonts/font-awesome-4.2.0/css/font-awesome.min.css" />
-		
-		<!--Parallax-->
-		<link rel="stylesheet" href="assets/css/style.css"/>
-		
-		<!--Custom-->
-		<link rel="stylesheet" href="assets/css/custom.css"/>
-		
-		<!--JQuery-->
-		<script type="text/javascript" src="assets/js/jquery-1.10.2.min.js"></script>
-		<!--Fonts-->
-		<link href='http://fonts.googleapis.com/css?family=Wellfleet' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Wellfleet' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Arvo:400,700,400italic,700italic' rel='stylesheet' type='text/css'>	
-		<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Goudy+Bookletter+1911' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Maven+Pro' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-
-
-
-		<!-- Start WOWSlider.com HEAD section -->
-		<link rel="stylesheet" type="text/css" href="engine1/style.css" />
-		<script type="text/javascript" src="engine1/jquery.js"></script>
-		<!-- End WOWSlider.com HEAD section -->
-
-		<style type="text/css">
-			.container {
-				min-height: 100vh;
-			}
-			p {
-				text-align: justify;
-			}
-			#slide1 {
-				padding-top: 10%;
-			}
-		</style>
-
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
-	<script>
-   
-<!----- JQUERY FOR SLIDING NAVIGATION --->   
-$(document).ready(function() {
-  $('a[href*=#]').each(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname
-    && this.hash.replace(/#/,'') ) {
-      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-       if ($target) {
-         var targetOffset = $target.offset().top;
-
-<!----- JQUERY CLICK FUNCTION REMOVE AND ADD CLASS "ACTIVE" + SCROLL TO THE #DIV--->   
-         $(this).click(function() {
-            $("#nav li a").removeClass("active");
-            $(this).addClass('active');
-           $('html, body').animate({scrollTop: targetOffset}, 1000);
-           return false;
-         });
-      }
-    }
-  });
-
-});
-
-
-</script>
-
-
- </head> 
+</head>
 
 <body>
 	<!-- HEADER START-->   
