63a14d0e8b2a156c1bec7166ed42f105259d60d1
Al Aji <alajiseno@gmail.com>
2015-08-06 13:07:58 +0700

    tes

--- application/views/register.php
+++ application/views/register.php
@@ -1,53 +1,332 @@
-<!DOCTYPE html>
-<!--
-	Company: Thinker Studio 
-	Customer: BUBW Indonesia
-	product: Website BUBW Indonesia
--->
-<html lang="en">
-  <?php 
+<head>
+	<?php
 	$this->load->view('head') ?>
-	
+
+	<style type="text/css">
+	.container {
+	min-height: 100vh;
+	}
+	p {
+	text-align: justify;
+	}
+	#slide1 {
+	padding-top: 10%;
+	}
+	</style>
+
+</head>
+
 <body>
 	<!-- HEADER START-->   
-		<!-- Navigation -->
-	    <nav class="navbar navbar navbar-fixed-top" role="navigation">
-	        <div class="container">
-	            <!-- Brand and toggle get grouped for better mobile display -->
-	            <div class="navbar-header">
-	                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-	                    <span class="sr-only">Toggle navigation</span>
-	                    <span class="icon-bar"></span>
-	                    <span class="icon-bar"></span>
-	                    <span class="icon-bar"></span>
-	                </button>
-	                <a class="navbar-brand" href="#slide1">
-	                    <h2 class="logo">BUBW<span><p>&nbspI N D O N E S I A</p></span></h2>
-	                </a>
-	            </div>
-	            <!-- Collect the nav links, forms, and other content for toggling -->
-	            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	                <ul class="nav navbar-nav pull-right">
-	                    <li>
-	                        <a href="#slide2" title="Greetings">Greetings</a>
-	                    </li>
-	                    <li>
-	                        <a href="#slide2ext" title="About">About</a>
-	                    </li>
-	                    <li>
-	                        <a href="#slide4" title="BUBW Indonesia">BUBW Indonesia</a>
-	                    </li>
-	                    <li>
-	                        <a href="#slide5" title="Registration">Registration</a>
-	                    </li>
-	                    <li>
-	                        <a href="#slide5" title="FAQ">FAQ</a>
-	                    </li>
-	                </ul>
-	            </div>
-	            <!-- /.navbar-collapse -->
-	        </div>
-	        <!-- /.container -->
-	    </nav>	
+	<!-- Navigation -->
+    <nav class="navbar navbar navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#slide0">
+                    <h2 class="logo">BUBW<span><p>&nbspI N D O N E S I A</p></span></h2>
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav pull-right">
+                    <li>
+                        <a href="<?php echo base_url('');?>welcome/index" title="Home">Back to main page</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>	
 	<!--HEADER END-->
+
+	<div id="slide1">
+		<div class="container">
+			<div class="page-header">
+	            <h1 style="text-align:left;">How to Apply?</h1>
+	        </div>
+	        <div class="col-md-8 col-sm-12">
+	        	<p>Better Understanding of Better World Indonesia open two different ways of registration, which are Call for Application and <strong>Call for Paper</strong>. Participants from <strong>Call for Application</strong> will need to submit online application only. Meanwhile, participants have responsibility to pay all accommodation during the conference and transportation from home to Yogyakarta. There will be no registration fee. If participants apply for Paper , they must submit online application form and paper related with our theme. Five best papers will be invited to the conference and present their ideas to BUBW conference. Chosen participants will not need to pay fee for accommodation during in Yogyakarta. Meanwhile, transportation from home to Yogyakarta will be self-funded by the participants.</p>
+				<br><br>
+				<h2>Registration Flow</h2>
+				<div class="col-md-6 col-sm-12">
+					<div class="panel panel-default">
+				    	<div class="panel-heading flow">
+				    		<h4>Call for Application</h4>
+				    	</div>
+				      	<ol class="list-group">
+				        	<li class="list-group-item">Download the Aplication Form in our website, <a href="http://www.bubwindonesia.com">bubwindonesia.com</a></li>
+				          	<li class="list-group-item">Fill in the Application Form</li>
+				          	<li class="list-group-item">Print this form after you fill in its entirety, and sign it with written signature</li>
+				          	<li class="list-group-item">Send a color scanned document of this form to: <a href="mailto:bubwindonesia@gmail.com"> bubwindonesia@gmail.com</a></li>
+				          	<li class="list-group-item">Participants will receive the email confirmation from the BUBW Indonesia Committee at maximum 48 hours after we received the email.</li>
+				          	<li class="list-group-item">Committee will send the <strong>Letter of Acceptance</strong> and the document requirement</li>
+				          	<li class="list-group-item">In the Letter of Acceptance, there will be Bank Account to transfer the fee of BUBW Indonesia. Fee rate will be published in advance.</li>
+				          	<li class="list-group-item">After you completely transfer the fee, we will give you an Invitation Letter to state that you are officially accepted as BUBW Indonesia participant.</li>
+				      	</ol>
+				  	</div>
+				</div>
+				<div class="col-md-6 col-sm-12">
+					<div class="panel panel-default">
+				    	<div class="panel-heading flow">
+				    		<h4>Call for Paper</h4>
+				    	</div>
+				      	<ol class="list-group">
+				        	<li class="list-group-item">Participants must pay the registration fee for each paper to BUBW account
+				        		<ul>
+				        			<li>National participant: IDR 300,000</li>
+				        			<li>International participant: USD 30</li>
+				        			<li>BUBW Account</li>
+				        		</ul>
+				        	</li>
+				          	<li class="list-group-item">Download the Aplication Form in our website, <a href="http://www.bubwindonesia.com">bubwindonesia.com</a></li>
+				          	<li class="list-group-item">Fill in the application form</li>
+				          	<li class="list-group-item">Print this form after you fill in its entirety, and sign it with written signature</a></li>
+				          	<li class="list-group-item">Paper requirements:</li>
+				          	<li class="list-group-item">Submit the scanned colored application, paper, and payment receipt to this email <a href="mailto:bubwindonesia@gmail.com"> bubwindonesia@gmail.com</a></li>
+				          	<li class="list-group-item">Fifteen best papers will be chosen and invited to BUBW conference in Yogyakarta, Indonesia</li>
+				          	<li class="list-group-item">Semifinalists must prepare a presentation about the papers (videos, photos, or other artifacts) to be delivered in the conference*</li>
+				          	<li class="list-group-item">Rate fee for Call for Paper participants will be published in advance</li>
+				          	<li class="list-group-item">There will be certificates and award categories for the winners</li>
+				      	</ol>
+				  	</div>
+				</div>
+				<br>
+		        <hr>
+				<br>
+
+
+				<h2>Eligibility</h2>
+				<ol class="list-group noborder">
+		        	<li class="list-group-item noborder">The participants are expected to be 18-30 years old highly motivated individual, who demonstrate leadership through academic work, community involvement and extracurricular activities.</li>
+		          	<li class="list-group-item noborder">Participants can be from any region in Indonesia and other country.</li>
+		          	<li class="list-group-item noborder">Proficient in English.</li>
+		          	<li class="list-group-item noborder">The participants should have a strong interest in interfaith and global understanding, pluralism, global citizenship, conflict resolution, and sustainable development.</a></li>
+		          	<li class="list-group-item noborder">The participant are suggested to have a proven track record of previous and current activities on interfaith and global understanding, pluralism, global citizenship, conflict resolution, and sustainable development.</li>
+		          	<li class="list-group-item noborder">Indicate a serious interest in learning about Indonesia with its pluralism.</li>
+		          	<li class="list-group-item noborder">Be mature, responsible, independent, confident, open-minded, tolerant, thoughtful and inquisitive.</li>
+		          	<li class="list-group-item noborder">Be willing and able to fully participate in the Better Understanding for Better World 2015 program.</li>
+		          	<li class="list-group-item noborder">Be comfortable and prepared to share living accommodations, able to make adjustment to cultural and social practices different from those of their home country.</li>
+		          	<li class="list-group-item noborder">International participants are highly recommended to have passports</li>
+		      	</ol>
+		      	<br>
+		        <hr>
+				<br>
+
+
+
+				<h2>Details of Payment</h2>
+				<p>You have to send us the digital copy of payment no longer than 10 days after you receive the Letter of Acceptance. Better Understanding for Better World (BUBW) Indonesia Fee is 300 USD.</p>
+				<h3>*Fee includes:</h3>
+				<ul>
+					<li>BUBW activities and programs</li>
+					<li>BUBW merchandises</li>
+					<li>Tour, interfaith, and cultural programs</li>
+					<li>5 days 4 nights stay at hotel</li>
+					<li>Transportation (arrival from airport to hotel, hotel to venue, any transportation needed during arranged programs, and departure from hotel to airport)</li>
+					<li>All times daily meals</li>
+					<li>Coffee break</li>
+				</ul>
+				<h3>*Fee does not includes:</h3>
+				<ul>
+					<li>Health insurance</li>
+					<li>Visa and travel</li>
+					<li>Bank transfer and/or correspondent bank charges</li>
+					<li>Souvenirs and shopping activities</li>
+					<li>Any additional charge that does not related with BUBW program</li>
+				</ul>
+				<h3>Details of Payment</h3>
+				<ol class="list-group noborder" style="margin-left:20px">
+		        	<li class="list-group-item noborder">After you accept Letter of Acceptance, you will be given bank account of BUBW Indonesia, then you have to pay BUBW Indonesia fee. Rate will be published in advance.</li>
+		          	<li class="list-group-item noborder">After you pay the fee, inform us via <a href="mailto:bubwindonesia@gmail.com"> bubwindonesia@gmail.com</a> immediately.</li>
+		          	<li class="list-group-item noborder">Scan the Application for Fund Transfer / Proof of Payment (the paper you get from bank as the proof that you’ve done the payment to BUBW Indonesia’s bank account number).</li>
+		          	<li class="list-group-item noborder">If you do not send the scanning in 10 days, we will remind you to send it in the next 5 days.</li>
+		          	<li class="list-group-item noborder">If after those 5 days you still do not send us your Application for Fund Transfer / Proof of Payment via email, we will list your name as waiting list for BUBW Indonesia.</li>
+		          	<li class="list-group-item noborder">If you’ve transferred the fee but you cancel your application as participant of BUBW Indonesia, there will be <strong>NO REFUND</strong>.</li>
+		        </ol>
+		        <br>
+		        <hr>
+				<br>
+
+		        <h2>Important Dates</h2>
+		        <h3>Call for Application</h3>
+		        <div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-md-8 ">
+							<ul class="event-list">
+								<li>
+									<time datetime="2014-07-20">
+										<span class="day">1</span>
+										<span class="month">Aug</span>
+										<span class="year">2015</span>
+										<span class="time">ALL DAY</span>
+									</time>
+									<div class="info">
+										<h2 class="title">Open Registration</h2>
+										<p class="desc">August 1st 2015</p>
+									</div>
+								</li>
+								<li>
+									<time datetime="2014-07-20">
+										<span class="day">1</span>
+										<span class="month">Aug</span>
+										<span class="year">2015</span>
+										<span class="time">ALL DAY</span>
+									</time>
+									<div class="info">
+										<h2 class="title">Early Seat</h2>
+										<p class="desc">Open for Early Seat start from August 1st - September 30th 2015</p>
+									</div>
+								</li>
+								<li>
+									<time datetime="2014-07-20">
+										<span class="day">30</span>
+										<span class="month">Sept</span>
+										<span class="year">2015</span>
+										<span class="time">ALL DAY</span>
+									</time>
+									<div class="info">
+										<h2 class="title">Late Seat</h2>
+										<p class="desc">Open for Late Seat start from September 30th - November 10th 2015</p>
+									</div>
+								</li>
+								<li>
+									<time datetime="2014-07-20">
+										<span class="day">10</span>
+										<span class="month">Nov</span>
+										<span class="year">2015</span>
+										<span class="time">ALL DAY</span>
+									</time>
+									<div class="info">
+										<h2 class="title">Extra Seat</h2>
+										<p class="desc">Open for Extra Seat start from November 10th - October 30th 2015 (Or will be announced later if there is quota left)</p>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+
+				<h3>Call for Paper</h3>
+		        <div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-md-8 ">
+							<ul class="event-list">
+								<li>
+									<time class="paper">
+										<span class="day">1</span>
+										<span class="month">Aug</span>
+										<span class="year">2015</span>
+										<span class="time">ALL DAY</span>
+									</time>
+									<div class="info">
+										<h2 class="title">Open Registration</h2>
+										<p class="desc">August 1st 2015</p>
+									</div>
+								</li>
+								<li>
+									<time class="paper">
+										<span class="day">1</span>
+										<span class="month">Aug</span>
+										<span class="year">2015</span>
+										<span class="time">ALL DAY</span>
+									</time>
+									<div class="info">
+										<h2 class="title">Online Registration</h2>
+										<p class="desc">Open for Online Registration start from August 1st - September 1st 2015</p>
+									</div>
+								</li>
+								<li>
+									<time class="paper">
+										<span class="day">1</span>
+										<span class="month">Sept</span>
+										<span class="year">2015</span>
+										<span class="time">ALL DAY</span>
+									</time>
+									<div class="info">
+										<h2 class="title">Submission of Paper</h2>
+										<p class="desc">Submission of Paper start from September 1st - September 20th 2015</p>
+									</div>
+								</li>
+								<li>
+									<time class="paper">
+										<span class="day">10</span>
+										<span class="month">Oct</span>
+										<span class="year">2015</span>
+										<span class="time">ALL DAY</span>
+									</time>
+									<div class="info">
+										<h2 class="title">Announcement (15 finalists)</h2>
+										<p class="desc">15 Finalist will be announced on October 10th 2015</p>
+									</div>
+								</li>
+								<li>
+									<time class="paper">
+										<span class="day">10</span>
+										<span class="month">Oct</span>
+										<span class="year">2015</span>
+										<span class="time">ALL DAY</span>
+									</time>
+									<div class="info">
+										<h2 class="title">Attendance Confirmation</h2>
+										<p class="desc">Attendance confirmation start from October 10th - October 30th 2015</p>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="col-md-4 col-sm-12" style="position:fixed; right:0">
+				<div class="container">
+					<h4>On going activity</h4>
+						<ul class="event-list">
+							<li>
+								<time>
+									<span class="day">1</span>
+									<span class="month">Aug</span>
+									<span class="year">2015</span>
+									<span class="time">ALL DAY</span>
+								</time>
+								<div class="info">
+									<h2 class="title">Open Registration</h2>
+									<p class="desc">Call for Application</p>
+									<button type="button" class="btn btn-default btn-lg" style="margin-left:10px;background-color:transparent">Register Now!</button>
+								</div>
+							</li>
+							<li>
+								<time class="paper">
+									<span class="day">1</span>
+									<span class="month">Aug</span>
+									<span class="year">2015</span>
+									<span class="time">ALL DAY</span>
+								</time>
+								<div class="info">
+									<h2 class="title">Online Registration</h2>
+									<p class="desc">Call for Paper</p>
+									<button type="button" class="btn btn-default btn-lg" style="margin-left:10px;background-color:transparent">Register Now!</button>
+								</div>
+							</li>
+						</ul>
+
+				</div>
+				
+			</div>
+
+
+	    </div>		
+	</div>
+		 <?php 
+			$this->load->view('footer') ?>
+        
 </body>
